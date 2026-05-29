0313e51734868424358d4526f099615d6cf95618
Siva2004 <sivavenkat140@gmail.com>
2026-05-29 12:19:47 +0530

    add superadmin management command

--- photoshop/create_admin.py
+++ photoshop/create_admin.py
@@ -2,9 +2,11 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
 class Command(BaseCommand):
+    help = 'Create superuser'
+
     def handle(self, *args, **kwargs):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin', 'your@email.com', 'admin123')
+        if not User.objects.filter(username='superadmin').exists():
+            User.objects.create_superuser('superadmin', 'itzzsiva05@gmail.com', 'super123')
             self.stdout.write('Superuser created!')
         else:
             self.stdout.write('Already exists.')
