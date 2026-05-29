e23bf81063ff261e0d90d96a6c8e2abc54b77eea
Siva2004 <sivavenkat140@gmail.com>
2026-05-29 15:04:58 +0530

    fix: add localhost to CSRF_TRUSTED_ORIGINS

--- PHOTOGHOR/settings.py
+++ PHOTOGHOR/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'photoghor.onrender.com']
-CSRF_TRUSTED_ORIGINS = ['https://photoghor.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://photoghor.onrender.com', 'http://127.0.0.1:8000']
 
 # =========================================================
 # INSTALLED APPS
